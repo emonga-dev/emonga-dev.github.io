b48fcafd063614de6c30c94bad36f5e25453c4a0
emonga <emonga99@gmail.com>
2025-06-08 15:58:35 +0900

    fix: initial browser-preference language
    - closes #27

--- src/logic/i18n/index.ts
+++ src/logic/i18n/index.ts
@@ -3,11 +3,23 @@ import { initReactI18next } from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+const availableLanguages = ['ko', 'en'];
+
+const getLanugage = () => {
+  const lang = navigator.language || 'ko-KR';
+  const shortLang = lang.split('-')[0];
+
+  return availableLanguages.includes(shortLang)
+    ? shortLang
+    : 'ko';
+};
+
 i18n
   .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    lng: getLanugage(),
     fallbackLng: 'ko',
     debug: true,
     interpolation: {
