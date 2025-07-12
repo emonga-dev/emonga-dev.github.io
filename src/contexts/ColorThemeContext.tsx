95fab40a1e88ee361169439e254272cb05d3ead8
emonga <emonga99@gmail.com>
2025-07-12 21:02:08 +0900

    fix: stash conflicts

--- src/contexts/ColorThemeContext.tsx
+++ src/contexts/ColorThemeContext.tsx
@@ -1,10 +1,5 @@
-<<<<<<< Updated upstream
-import { palette } from '../style/palette.ts';
-import { createContext, ReactNode, useContext, useState } from 'react';
-=======
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useState } from 'react';
 import { ColorTheme, ColorThemeContext, defaultTheme } from './ColorThemeContext.ts';
->>>>>>> Stashed changes
 
 const ColorThemeProvider = ({ children }: { children: ReactNode }) => {
   const [ theme, setTheme ] = useState<ColorTheme>(defaultTheme);
